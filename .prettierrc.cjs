2e82aa59ee167aea457505dc1c7f3ef2c5b6f243
Tano Gianatiempo <agianatiempo@systemone.id>
2023-01-03 20:17:58 -0300

    updated to antd

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  singleQuote: true,
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+  importOrder: [
+    '^vite',
+    '^react',
+    '^antd',
+    'components/',
+    'pages/',
+    'hooks/',
+    'utils/',
+    '^[./]',
+  ],
+  importOrderSortSpecifiers: true,
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderCaseInsensitive: true,
+};
