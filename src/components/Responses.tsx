622c33911721dc26dc3a44a383ff61a4cc4f512c
Ariel Gianatiempo <gianatiempo@gmail.com>
2024-01-17 12:11:08 -0300

    check facturas

--- src/components/Responses.tsx
+++ src/components/Responses.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, Row, Statistic } from 'antd';
+import { Card, Checkbox, Col, Row, Statistic } from 'antd';
 
 type ResponsesProps = {
   facturas: number[];
@@ -9,8 +9,14 @@ const Responses = ({ facturas }: ResponsesProps): JSX.Element => {
     <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
       {facturas.map((f, i) => (
         <Col xs={24} sm={12} md={8} lg={6} key={`${f}-${i}`}>
-          <Card style={{ marginBottom: 8 }}>
-            <Statistic title={`Factura #${i + 1}`} value={toARS(f)} valueStyle={{ color: '#3f8600' }} />
+          <Card
+            style={{ marginBottom: 8 }}
+            extra={<Checkbox />}
+            headStyle={{ borderBottom: 'none' }}
+            title={`Factura #${i + 1}`}
+            bodyStyle={{ padding: '0px 24px 12px' }}
+          >
+            <Statistic value={toARS(f)} valueStyle={{ color: '#3f8600' }} />
           </Card>
         </Col>
       ))}
